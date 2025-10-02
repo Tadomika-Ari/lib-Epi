09371c7924e1ac9be30ddcc5a547eabe341d45b5
EECKHOUTTE Lucas <lucas1.eeckhoutte@epitech.eu>
2025-10-02 09:21:13 +0200

    add my_compute_square.c

--- dossier lib/lib/my/my_compute_square_root.c
+++ dossier lib/lib/my/my_compute_square_root.c
@@ -7,5 +7,13 @@
 
 int my_compute_square_root(int nb)
 {
+    if (nb < 0) {
+        return 0;
+    }
+    for (int i = 0; i <= nb; i++) {
+        if (i * i == nb) {
+            return i;
+        }
+    }
     return 0;
 }
