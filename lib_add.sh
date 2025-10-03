f3213f16797d951842186954b0b0ea0e36fc55d5
EECKHOUTTE Lucas <lucas1.eeckhoutte@epitech.eu>
2025-10-03 09:49:57 +0200

    correction des derniers bugs connues

--- lib_add.sh
+++ lib_add.sh
@@ -1,10 +1,10 @@
-wget https://github.com/Tadomika-Ari/lib-Epi/archive/refs/heads/main.zip
+wget https://github.com/Tadomika-Ari/lib-Epitech/archive/refs/heads/main.zip
 unzip 'main.zip'
 rm main.zip
-cd "lib-Epi-main"
+cd "lib-Epitech-main"
 cd "dossier lib"
 cp -r lib "../../"
 cp -r include "../.."
 cd ..
 cd ..
-rm -rf "lib-Epi-main"
+rm -rf "lib-Epitech-main"
