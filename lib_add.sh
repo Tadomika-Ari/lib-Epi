29d1c15b42ced0737657e47ccf6d9c636b2dcdd0
EECKHOUTTE Lucas <lucas1.eeckhoutte@epitech.eu>
2025-10-08 23:12:41 +0200

    modif nom dossier dans script

--- lib_add.sh
+++ lib_add.sh
@@ -2,7 +2,7 @@ wget https://github.com/Tadomika-Ari/lib-Epitech/archive/refs/heads/main.zip
 unzip 'main.zip'
 rm main.zip
 cd "lib-Epitech-main"
-cd "dossier lib"
+cd "dossier_lib"
 cp -r lib "../../"
 cp -r include "../.."
 cd ..
